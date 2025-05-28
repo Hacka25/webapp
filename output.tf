2ff56a3d43c1d39cbe584fb063f83d80466217b4
Danish584 <danish.biyabani@gmail.com>
2025-05-28 19:32:09 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "gke_cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
 output "sql_instance_connection_name" {
   value = google_sql_database_instance.mysql_instance.connection_name
 }
